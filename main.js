6c9f5e08ce4f471ba7621c8a0bbcbbdbfc0f16b6
Nevan Catoy <93510179+ncvyn@users.noreply.github.com>
2024-01-25 22:54:27 +0800

    Add Alpine
    Alpine.js because yes

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
-document.querySelector('#app').innerHTML = /*html*/ `
-  <h1 id="numbers">...</h1>
-  <input autofocus autocomplete="off" type="number" pattern="[0-9]*" id="input">
-`;
-
-let input = document.getElementById('input');
-let numbersEle = document.getElementById('numbers');
-
-let numbers = Array.from(Array(5), (_) => randomInteger(10, 50));
-numbersEle.innerHTML = numbers.join('+');
-
-let correctAnswer = numbers.reduce((acc, x) => {
-  return acc + x;
-});
-
-input.addEventListener('input', (_) => {
-  input.value = input.value.replace('e', '');
-
-  if (input.value == correctAnswer) {
-    alert('good job');
-  }
-});
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
 
 /**
  *
@@ -30,3 +10,33 @@ input.addEventListener('input', (_) => {
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+let numbers = Array.from(Array(5), (_) => randomInteger(1, 50));
+
+Alpine.data('data', () => ({
+  numbers: numbers,
+  numbersText: numbers.join('+'),
+  answer: numbers.reduce((acc, x) => {
+    return acc + x;
+  }),
+
+  onInput(element) {
+    if (element.value == this.answer) {
+      numbers = Array.from(Array(5), (_) => randomInteger(1, 50));
+      this.numbers = numbers;
+      this.numbersText = this.numbers.join('+');
+      this.answer = numbers.reduce((acc, x) => {
+        return acc + x;
+      });
+    }
+  },
+}));
+
+document.querySelector('#app').innerHTML = /*html*/ `
+  <div x-data="data">
+    <h1 x-text="numbersText" id="numbers">...</h1>
+    <input id="input" @input="onInput($el)" autofocus autocomplete="off" type="number" pattern="[0-9]*" >
+  </div>
+`;
+
+Alpine.start();
