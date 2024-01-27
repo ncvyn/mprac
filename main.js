e2dd9cfc9c85ba226642c649e65f0ad97a5e5106
Nevan Catoy <93510179+ncvyn@users.noreply.github.com>
2024-01-27 19:27:45 +0800

    Move HTML and function

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
-document.querySelector('#app').innerHTML = /*html*/ `
-  <div x-data="data">
-    <h1 x-text="numbersText" id="numbers">...</h1>
-    <input id="input" @input="onInput($el)" autofocus autocomplete="off" type="number" pattern="[0-9]*" >
-  </div>
-`;
-
 import Alpine from 'alpinejs';
 window.Alpine = Alpine;
-
-/**
- *
- * @param {int} min
- * @param {int} max
- * @returns {int}
- */
-function randomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 let numbers = Array.from(Array(5), (_) => randomInteger(1, 50));
 
@@ -42,3 +25,13 @@ Alpine.data('data', () => ({
 }));
 
 Alpine.start();
+
+/**
+ *
+ * @param {int} min
+ * @param {int} max
+ * @returns {int}
+ */
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
